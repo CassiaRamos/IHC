151545839d1742453bc70fe1bad4d8f3fc66f611
MATEUS  SILVA <32516399+freakln@users.noreply.github.com>
2019-05-27 01:15:33 -0300

    validacao
    validação campos

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,14 +1,37 @@
 
 
-function valida_campo (campo){
-    if(document.getElementById(campo).value.length < 1){
-       
-    alert("Por favor, preencha o campo Horas");
-    alert("Caso não seja utilizado gentileza informar Zero");
 
-    document.getElementById(campo).focus();
-    return false
+
+
+
+
+
+
+
+
+function valida_campo (){
+
+    var campo = ["wApp","insTime","fbTime","twTime","youTime","outras","inputIdade"];
+    var campoLabel = ["What'up","Instagram","Face Book","Twitter","You tube","Outras rede Socias","Tempo redes socias"];
+    var result = "Favor preencha(s) os campos ";
+    var aux=0;
+
+    for (let i = 0; i <campo.length ; i++) {
+        if (document.getElementById(campo[i]).value.length < 1) {
+            result+= campoLabel[i]+", ";
+            aux++;
+        }
     }
+    if(aux>0) {
+        alert(result);
+        return false;
+    }else{
+
+        return true;
+    }
+
+
+
     }
 
 
@@ -21,35 +44,53 @@ function valida_campo (campo){
 
 
 function myFunction() {
-
- var tempoZap = timeStringToFloat(document.getElementById("wApp").value) ;
- var tempoInst = timeStringToFloat(document.getElementById("insTime").value) ;
- var tempoFB = timeStringToFloat(document.getElementById("fbTime").value) ;
- var tempoTW = timeStringToFloat(document.getElementById("twTime").value) ;
- var tempoYT = timeStringToFloat(document.getElementById("youTime").value) ;
- var tempoOutras = timeStringToFloat(document.getElementById("outras").value) ;
- var idade = timeStringToFloat(document.getElementById("idade").value) ;
-
-if(tempoFB >=0 && tempoInst >= 0 && tempoZap >=0 && tempoTW >=0 && tempoYT >=0 && tempoOutras >=0 && idade >=0){
+    var campo = ["wApp","insTime","fbTime","twTime","youTime","outras","inputIdade"];
 
 
-    var soma=tempoZap+tempoInst+tempoFB+tempoTW+tempoYT+tempoOutras;
-    console.log(soma);
-    if(soma>24){
-        alert("Não é possivel dias com mais de 24 horas");
-    }else if(soma<3){
-        
-        window.location.href = "nao-dependente.html";
-    }else if(soma<6){
-        window.location.href = "moderado.html";
-    
-    }else{
-        window.location.href = "muito-dependente.html";
+
+    if(valida_campo()){
+        var tempoZap = timeStringToFloat(document.getElementById(campo[0]).value);
+        var tempoInst = timeStringToFloat(document.getElementById(campo[1]).value);
+        var tempoFB = timeStringToFloat(document.getElementById(campo[2]).value);
+        var tempoTW = timeStringToFloat(document.getElementById(campo[3]).value);
+        var tempoYT = timeStringToFloat(document.getElementById(campo[4]).value);
+        var tempoOutras = timeStringToFloat(document.getElementById(campo[5]).value);
+        var idade = timeStringToFloat(document.getElementById(campo[6]).value);
+
+        if(tempoFB >=0 && tempoInst >= 0 && tempoZap >=0 && tempoTW >=0 && tempoYT >=0 && tempoOutras >=0 && idade >=0){
+
+
+            var soma=tempoZap+tempoInst+tempoFB+tempoTW+tempoYT+tempoOutras;
+            console.log(soma);
+            if(soma>24){
+                alert("Não é possivel dias com mais de 24 horas");
+            }else if(soma<3){
+
+                window.location.href = "nao-dependente.html";
+            }else if(soma<6){
+                window.location.href = "moderado.html";
+
+            }else{
+                window.location.href = "muito-dependente.html";
+
+            }
+        } else {
+            alert("Certifique-se de preencher todos os campos corretamente.");
+        }
+
+
 
     }
-} else {
-    alert("Certifique-se de preencher todos os campos corretamente.");
-}
+
+
+
+
+
+
+
+
+
+
 }
 
 
